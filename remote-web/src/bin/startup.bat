1141df625cdf7bad767e3ae816772bcd39452965
JiaZhiFeng <312290710@qq.com>
2021-12-09 15:34:04 +0800

    update:修改启动脚本字符编码

--- remote-web/src/bin/startup.bat
+++ remote-web/src/bin/startup.bat
@@ -1,4 +1,4 @@
 @echo off
 @chcp.com 65001 > NUL 
-java -server -Xms1G -Xmx1G -Xmn256m -cp "../config/;../lib/*" com.jzf.remote.web.RemoteWebApplication
+java -server -Xms1G -Xmx1G -Xmn256m -Dfile.encoding=UTF-8 -cp "../config/;../lib/*" com.jzf.remote.web.RemoteWebApplication
 pause
