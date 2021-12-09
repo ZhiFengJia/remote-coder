e417766c38859cae6e9b0f75cff00575766a1b84
JiaZhiFeng <312290710@qq.com>
2021-12-09 23:12:19 +0800

    update:修改页面样式

---
+++ remote-web/src/main/resources/sourceCodeFiles/algorithm/src/test/java/com/jzf/algorithm/Test.java
@@ -0,0 +1,12 @@
+package com.jzf.algorithm
+/**
+ * Test
+ *
+ * @author jiazhifeng
+ * @date 2021/12/09 21:58
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
