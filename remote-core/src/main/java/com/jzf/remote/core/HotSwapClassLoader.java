d7d4dda0ab596b43843480e7ec5b8de4b4814ef5
JiaZhiFeng <312290710@qq.com>
2021-12-10 11:28:32 +0800

    fix:保存动态编译的class文件

--- remote-core/src/main/java/com/jzf/remote/core/HotSwapClassLoader.java
+++ remote-core/src/main/java/com/jzf/remote/core/HotSwapClassLoader.java
@@ -1,9 +1,11 @@
 package com.jzf.remote.core;
 
 import com.jzf.remote.core.util.Constants;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -31,8 +33,8 @@ public class HotSwapClassLoader extends ClassLoader {
         String userDir = System.getProperty("user.dir");
         String nameSuffix = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
         String path = userDir + "/../dynamic_compiler_files/" + name + "_" + nameSuffix + ".class";
-        LOG.info("动态编译存储路径:{}", path);
-        try (FileOutputStream fos = new FileOutputStream(path)) {
+        LOG.info("保存动态编译文件路径:{}", path);
+        try (FileOutputStream fos = FileUtils.openOutputStream(new File(path))) {
             fos.write(modiBytes);
         } catch (IOException e) {
             LOG.error(e.getMessage());
