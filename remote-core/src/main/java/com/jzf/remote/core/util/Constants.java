bb44223bace4f972f4d7746765f514e93bb3b6a4
jiazhifeng <wb.jiazhifeng01@mesg.corp.netease.com>
2021-12-22 20:11:12 +0800

    refactor:重构项目结构

--- remote-core/src/main/java/com/jzf/remote/core/util/Constants.java
+++ remote-core/src/main/java/com/jzf/remote/core/util/Constants.java
@@ -3,7 +3,7 @@ package com.jzf.remote.core.util;
 public class Constants {
     public static final String SYSTEM_CLASS = "com/jzf/remote/core/JZFSystem";
     public static final String WORK_DIR = System.getProperty("user.dir") + "/../";
-    public static final String SOURCE_DIR = WORK_DIR + "source_code_files/";
+    public static final String SOURCE_DIR = WORK_DIR + "workspace/";
     public static final String COMPILED_DIR = WORK_DIR + "dynamic_compiler_files/";
 
 }
